a514a1c477b984be7a42a0bcdb1dfa93ab636968
Srini <maddinenisri@gmail.com>
2015-08-31 08:28:31 -0400

    Added static html

---
+++ loan_reporter_play/app/controllers/HomeController.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object HomeController extends Controller {
+  def index = Action {
+    Ok(views.html.index("Your new application is ready. Message from core: Srini"))
+  }
+}
